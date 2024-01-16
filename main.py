8f290b6fef3fbc56233689bdecd75cc57af6830c
Your Name <you@example.com>
2024-01-16 22:12:34 +0545

    xlsss

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd 
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-excel_file_path = os.path.join(current_dir, 'routine.xls.xlsx')
+excel_file_path = os.path.join(current_dir, 'routine.xlsx')
 routine = pd.read_excel(excel_file_path)
 
 cell_value = routine.iloc[0,1]
