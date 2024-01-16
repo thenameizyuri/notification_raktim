372cc558d6c57e0eb7df5decb83011b1de8bc110
Your Name <you@example.com>
2024-01-16 22:07:26 +0545

    xlsss

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("HEllo world ")
+import os 
+import pandas as pd 
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+excel_file_path = os.path.join(current_dir, 'routine.xls.xlsx')
+routine = pd.read_excel(excel_file_path)
+
+cell_value = routine.iloc[0,1]
+print(f'the value of the first cell is {cell_value}')
+
+# print(routine)
+
+
+
